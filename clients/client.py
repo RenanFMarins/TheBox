2a8147dd8f0c573635d8efe6cf1e146f88b4d596
RenanFMarins <renan.fmarins@gmail.com>
2023-06-16 15:09:27 -0300

    Cliente

---
+++ clients/client.py
@@ -0,0 +1,16 @@
+class Client():
+    identificador = 1
+    
+    def __init__(self, nome, sobrenome, cpf, banco):
+        self.id = Client.identificador 
+        Client.identificador += 1
+        self.nome = nome
+        self.sobrenome = sobrenome
+        self.cpf = cpf
+        self.banco = banco
+    
+    def __str__(self):
+        return (f'O id do cliente é: {self.id}\n'
+                f'O nome do cliente: {self.nome} {self.sobrenome}\n'
+                f'CPF: {self.cpf}\n')
+    
